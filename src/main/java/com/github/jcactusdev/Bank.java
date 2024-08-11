7e45a640db4f71725a0e28ca230682431709d4f0
JCactus <dev.jcactus@gmail.com>
2024-08-11 20:35:25 +0500

    First commit

---
+++ src/main/java/com/github/jcactusdev/Bank.java
@@ -0,0 +1,151 @@
+package com.github.jcactusdev;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank {
+    private final int number;
+
+    private final List<BankAccount> accounts;
+    private final Lock bankLock;
+
+    public Bank(int number, int accountCount) {
+        this(number, accountCount, 0);
+    }
+
+    public Bank(int number, int accountCount, int startBalance) {
+        this.number = number;
+        accounts = new ArrayList<>(accountCount);
+        for (int i = 0; i < accountCount; i++) {
+            accounts.add(new BankAccount(i, startBalance));
+        }
+        bankLock = new ReentrantLock();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+
+    public BankAccount getAccount(int i) {
+        bankLock.lock();
+        try {
+            return accounts.get(i);
+        }
+        finally {
+            bankLock.unlock();
+        }
+    }
+
+    public synchronized int getBalance(int from) {
+        bankLock.lock();
+        try {
+            return accounts.get(from).getBalance();
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public synchronized int getTotalBalance() {
+        bankLock.lock();
+        try {
+            return accounts.stream().mapToInt(BankAccount::getBalance).sum();
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public int size() {
+        return accounts.size();
+    }
+
+    public synchronized void transfer(int fromAccountId, int toAccountId, int amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            accounts.get(fromAccountId).deposit(-amount);
+            System.out.printf(" %d from bank[%d] account[%d] to bank[%d], account[%d]", amount, number, fromAccountId, number, toAccountId);
+            accounts.get(toAccountId).deposit(amount);
+            System.out.printf(" to Balance: %d\n", getBalance(toAccountId));
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public void transferEverythingToAccount(int toAccountId) {
+        accounts.stream().filter(a -> a.getNumber() != toAccountId).forEach(account -> {
+            try {
+                transfer(account.getNumber(), toAccountId, account.getBalance());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    public synchronized void transfer(int fromAccountId, Bank toBank, int toAccountId, int amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            accounts.get(fromAccountId).deposit(-amount);
+            System.out.printf(" %d from bank[%d] account[%d] to bank[%d], account[%d]", amount, number, fromAccountId, toBank.getNumber(), toAccountId);
+            toBank.getAccount(toAccountId).deposit(amount);
+            System.out.printf(" to Balance: %d\n", toBank.getBalance(toAccountId));
+        }
+        finally {
+            bankLock.unlock();
+        }
+    }
+
+    public static class BankAccount {
+        private final int number;
+        private final Lock accountLock;
+        private volatile int balance = 0;
+
+        public BankAccount(int number) {
+            this(number, 0);
+        }
+
+        public BankAccount(int number, int startBalance) {
+            this.number = number;
+            this.accountLock = new ReentrantLock();
+            this.balance = startBalance;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public synchronized int getBalance() {
+            accountLock.lock();
+            try {
+                return balance;
+            } finally {
+                accountLock.unlock();
+            }
+        }
+
+        public void setBalance(int balance) {
+            accountLock.lock();
+            try {
+                this.balance = balance;
+            } finally {
+                accountLock.unlock();
+            }
+        }
+
+        public synchronized void deposit(int amount) {
+            accountLock.lock();
+            try {
+                balance += amount;
+            } finally {
+                accountLock.unlock();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/github/jcactusdev/Main.java
@@ -0,0 +1,46 @@
+package com.github.jcactusdev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Thread.sleep;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Bank> banks = new ArrayList<>(3);
+        banks.add(new Bank(0, 1500, 1));
+        banks.add(new Bank(1, 2000, 2));
+        banks.add(new Bank(2, 3000, 3));
+
+        Bank toBank = new Bank(777, 3, 10);
+
+        banks.forEach(bank -> {
+            for (Bank.BankAccount account : bank.getAccounts()) {
+                if (account.getBalance() > 0) {
+                    try {
+                        bank.transfer(account.getNumber(), toBank, 0, 1);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        });
+
+        System.out.println(getTotalBalancesFromBanks(banks));
+        System.out.println(toBank.getTotalBalance());
+        System.out.println(toBank.getBalance(0));
+
+        toBank.transferEverythingToAccount(0);
+        System.out.println(toBank.getBalance(0));
+    }
+
+    public static int getTotalBalancesFromBanks(List<Bank> banks) {
+        return banks.stream().mapToInt(Bank::getTotalBalance).sum();
+    }
+}
+
+
