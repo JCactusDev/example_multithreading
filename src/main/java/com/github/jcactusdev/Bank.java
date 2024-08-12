9e819bb7ede09be8b362a1faa0c7b389c3ce750d
JCactus <dev.jcactus@gmail.com>
2024-08-12 11:05:32 +0500

    HW11 clear trash

--- src/main/java/com/github/jcactusdev/Bank.java
+++ src/main/java/com/github/jcactusdev/Bank.java
@@ -2,14 +2,10 @@ package com.github.jcactusdev;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
     private final int number;
-
     private final List<BankAccount> accounts;
-    private final Lock bankLock;
 
     public Bank(int number, int accountCount) {
         this(number, accountCount, 0);
@@ -21,7 +17,6 @@ public class Bank {
         for (int i = 0; i < accountCount; i++) {
             accounts.add(new BankAccount(i, startBalance));
         }
-        bankLock = new ReentrantLock();
     }
 
     public int getNumber() {
@@ -33,48 +28,34 @@ public class Bank {
     }
 
     public BankAccount getAccount(int i) {
-        bankLock.lock();
-        try {
-            return accounts.get(i);
-        } finally {
-            bankLock.unlock();
-        }
+        return accounts.get(i);
     }
 
-    public synchronized int getBalance(int from) {
-        bankLock.lock();
-        try {
-            return accounts.get(from).getBalance();
-        } finally {
-            bankLock.unlock();
-        }
+    public long getBalance(int from) {
+        return accounts.get(from).getBalance();
     }
 
-    public synchronized int getTotalBalance() {
-        bankLock.lock();
-        try {
-            return accounts
-                    .stream()
-                    .mapToInt(BankAccount::getBalance)
-                    .sum();
-        } finally {
-            bankLock.unlock();
-        }
+    public long getTotalBalance() {
+        return accounts
+                .stream()
+                .mapToLong(BankAccount::getBalance)
+                .sum();
     }
 
     public int size() {
         return accounts.size();
     }
 
-    public synchronized void transfer(int fromAccountId, int toAccountId, int amount) throws InterruptedException {
-        bankLock.lock();
-        try {
-            accounts.get(fromAccountId).deposit(-amount);
-            accounts.get(toAccountId).deposit(amount);
-            System.out.printf(" %d from bank[%d], account[%d] to bank[%d], account[%d] to Balance: %d\n", amount, number, fromAccountId, number, toAccountId, getBalance(toAccountId));
-        } finally {
-            bankLock.unlock();
-        }
+    public void transfer(int fromAccountId, int toAccountId, long amount) throws InterruptedException {
+        accounts.get(fromAccountId).deposit(-amount);
+        accounts.get(toAccountId).deposit(amount);
+        System.out.printf(" %d from bank[%d], account[%d] to bank[%d], account[%d] to Balance: %d\n", amount, number, fromAccountId, number, toAccountId, getBalance(toAccountId));
+    }
+
+    public void transfer(int fromAccountId, Bank toBank, int toAccountId, long amount) throws InterruptedException {
+        accounts.get(fromAccountId).deposit(-amount);
+        toBank.getAccount(toAccountId).deposit(amount);
+        System.out.printf(" %d from bank[%d], account[%d] to bank[%d], account[%d] to Balance: %d\n", amount, number, fromAccountId, toBank.getNumber(), toAccountId, toBank.getBalance(toAccountId));
     }
 
     public void transferEverythingToAccount(int toAccountId) {
@@ -91,21 +72,9 @@ public class Bank {
                 );
     }
 
-    public synchronized void transfer(int fromAccountId, Bank toBank, int toAccountId, int amount) throws InterruptedException {
-        bankLock.lock();
-        try {
-            accounts.get(fromAccountId).deposit(-amount);
-            toBank.getAccount(toAccountId).deposit(amount);
-            System.out.printf(" %d from bank[%d], account[%d] to bank[%d], account[%d] to Balance: %d\n", amount, number, fromAccountId, toBank.getNumber(), toAccountId, toBank.getBalance(toAccountId));
-        } finally {
-            bankLock.unlock();
-        }
-    }
-
     public static class BankAccount {
         private final int number;
-        private final Lock accountLock;
-        private volatile int balance = 0;
+        private volatile long balance = 0;
 
         public BankAccount(int number) {
             this(number, 0);
@@ -113,7 +82,6 @@ public class Bank {
 
         public BankAccount(int number, int startBalance) {
             this.number = number;
-            this.accountLock = new ReentrantLock();
             this.balance = startBalance;
         }
 
@@ -121,31 +89,12 @@ public class Bank {
             return number;
         }
 
-        public synchronized int getBalance() {
-            accountLock.lock();
-            try {
-                return balance;
-            } finally {
-                accountLock.unlock();
-            }
+        public synchronized long getBalance() {
+            return balance;
         }
 
-        public void setBalance(int balance) {
-            accountLock.lock();
-            try {
-                this.balance = balance;
-            } finally {
-                accountLock.unlock();
-            }
-        }
-
-        public synchronized void deposit(int amount) {
-            accountLock.lock();
-            try {
-                balance += amount;
-            } finally {
-                accountLock.unlock();
-            }
+        public synchronized void deposit(long amount) {
+            balance += amount;
         }
     }
 
--- src/main/java/com/github/jcactusdev/Main.java
+++ src/main/java/com/github/jcactusdev/Main.java
@@ -6,14 +6,12 @@ import java.util.List;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static java.lang.Thread.sleep;
-
 public class Main {
 
     private static final List<Bank> banks = new ArrayList<>(Arrays.asList(
-            new Bank(0, 1000, 2),
-            new Bank(1, 1000, 2),
-            new Bank(2, 1000, 2)
+            new Bank(0, 10000, 10000000),
+            new Bank(1, 10000, 10000000),
+            new Bank(2, 10000, 10000000)
     ));
     private static final Bank targetBank = new Bank(777, 10);
     private static final CyclicBarrier BARRIER = new CyclicBarrier(banks.size(), new TransferCompleted(banks, targetBank));
@@ -26,10 +24,10 @@ public class Main {
 
     }
 
-    public static int getTotalBalancesFromBanks(List<Bank> banks) {
+    public static long getTotalBalancesFromBanks(List<Bank> banks) {
         return banks
                 .stream()
-                .mapToInt(Bank::getTotalBalance)
+                .mapToLong(Bank::getTotalBalance)
                 .sum();
     }
 
@@ -51,7 +49,9 @@ public class Main {
                             .filter(account -> account.getBalance() > 0)
                             .forEach(account -> {
                                 try {
-                                    bank.transfer(account.getNumber(), targetBank, ThreadLocalRandom.current().nextInt(0, targetBank.size()), 1);
+                                    long currentBalance = bank.getBalance(account.getNumber());
+                                    long amount = currentBalance == 1 ? currentBalance : ThreadLocalRandom.current().nextLong(1, currentBalance);
+                                    bank.transfer(account.getNumber(), targetBank, ThreadLocalRandom.current().nextInt(0, targetBank.size()), amount);
                                 } catch (InterruptedException e) {
                                     Thread.currentThread().interrupt();
                                 }
@@ -76,9 +76,9 @@ public class Main {
         @Override
         public void run() {
             // Status
-            System.out.printf("Total balance from banks - %d\n", getTotalBalancesFromBanks(banks));
-            System.out.printf("Total balance in target bank - %d\n", targetBank.getTotalBalance());
-            System.out.printf("Account '0' balance in target bank[%d] - %d\n", targetBank.getNumber(), targetBank.getBalance(0));
+            System.out.printf("Total balance in 'banks' - %d\n", getTotalBalancesFromBanks(banks));
+            System.out.printf("Total balance in target bank[%d] - %d\n", targetBank.getNumber(), targetBank.getTotalBalance());
+            System.out.printf("Account '0' balance in 'target bank[%d] - %d\n", targetBank.getNumber(), targetBank.getBalance(0));
 
             // Transfer everything to account '0' in target bank
             if (targetBank.getTotalBalance() > targetBank.getBalance(0)) {
@@ -89,5 +89,3 @@ public class Main {
     }
 
 }
-
-
